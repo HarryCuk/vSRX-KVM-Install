119c3123e2a8874c6fa28339694edcce15dc9ee6
Harry Claridge <37987433+HarryCuk@users.noreply.github.com>
2022-11-02 16:49:03 +0000

    add vsrx_create_vm script

---
+++ vsrx_create_vm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -n "Mounting vSRX image..."
+# Copy and mount vSRX image.
+cp junos-vsrx3-x86-64-22.3R1.11.qcow2 /mnt/vsrx20one.qcow2
+sleep 2
+
+echo -n "Creating virtual machine../"
+# vSRX virtual machine installation command.
+  # --name <name of the Virtual Machine>.
+  # --ram <define the amount of RAM to allocate to the VM>.
+  # --cpu <define the CPU model>. See cpu_map.xml file for full list, or use host to automatically use the host CPU.
+  # --vcpus <define the number of vCPU's to allocate to the VM.>
+  # --arch <define the compute architecture>. x86_64 is the most common. If removed the host CPU architecture will be used.
+  # --disk <define the path to the vSRX image> size <HDD size>, bus<Storage bus>, format<type of VM file>
+  # --os-variant<specify the operating system of the GUEST.>
+  # --import <specify the networks to be attached to the VM.>
+virt-install --name vSrxTest \
+ --memory 4096 \
+ --cpu host \
+ --vcpus=2 \
+ --arch=x86_64 \
+ --disk /mnt/vsrx20one.qcow2,size=16,device=disk,bus=ide,format=qcow2 --import \
+  --os-type linux \
+  --os-variant rhel7.0 \
+  --network=network:default,model=virtio \
+  --network=network:trusted,model=virtio \
+  --network=network:untrusted,model=virtio
+
